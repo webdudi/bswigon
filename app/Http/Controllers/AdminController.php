d3fc3a41968c70ef804b5adf0c88248bc22b6c28
bswigon <swigon.bartlomiej@gmail.com>
2017-11-18 12:25:56 +0100

    Dodanie panelu admina

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use View;
+use App\Lead;
+use Auth;
+
+class AdminController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return View::make('admin.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+
+    public function logout()
+    {
+        Auth::logout();
+        return redirect('/');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,24 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+/* FORMS */
+
+Route::get('/', ['as' => 'index', 'uses' => 'SystemController@index']);
+Route::get('/home', ['as' => 'home', 'uses' => 'SystemController@index']);
+
+/* ADMIN */
+
+Auth::routes();
+
+Route::get('logout', ['as' => 'logout', 'uses' => 'AdminController@logout']);
+
+Route::group(['prefix' => 'admin'], function () {
+    Route::get('/', ['as' => 'home', 'uses' => 'AdminController@index'])->middleware('auth');
+    Route::post('update', ['as' => 'lead_update', 'uses' => 'AdminController@update'])->middleware('auth');
+    Route::get('show/{id}', ['as' => 'lead_edit', 'uses' => 'AdminController@show'])->middleware('auth');
+    Route::get('edit/{id}', ['as' => 'lead_edit', 'uses' => 'AdminController@edit'])->middleware('auth');
+    Route::get('send/{id}', ['as' => 'lead_edit', 'uses' => 'AdminController@send'])->middleware('auth');
+    Route::get('destroy/{id}', ['as' => 'lead_destroy', 'uses' => 'AdminController@destroy'])->middleware('auth');
 });
+
+//Route::get('register', ['as' => 'register', 'uses' => 'LeadsController@create']);
